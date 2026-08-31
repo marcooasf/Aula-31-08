026117535bd3cb6dd7cc2e4916f0fa9b03a5fd0b
Cursos Oficiais Microsoft <moc@proway.treina>
2026-08-31 20:52:49 -0300

    atualizacao

---
+++ API-31-08/Services/EnderecoService.cs
@@ -0,0 +1,50 @@
+﻿using API_31_08.Models;
+using API_31_08.Repositories;
+
+namespace API_31_08.Services;
+public interface IEnderecoService
+{
+    Task<List<Endereco?>> ListarPorFuncionarioAsync(int funcionarioId);
+    Task<Endereco?> ObterPorIdAsync(int id);
+    Task<Endereco?> CriarAsync(int funcionarioId, Endereco endereco);
+    Task<Endereco?> AtualizarAsync(int id, Endereco endereco);
+    Task<bool> RemoverAsync(int id);
+}
+
+public class EnderecoService : IEnderecoService
+{
+    public readonly IEnderecoRepository _repository;
+    public readonly IFuncionarioRepository _funcionarioRepository;
+    
+    public EnderecoService(IEnderecoRepository repository,  IFuncionarioRepository funcionarioRepository)
+    {
+        _repository = repository;
+        _funcionarioRepository = funcionarioRepository;
+    }
+    
+    public async Task<List<Endereco?>> ListarPorFuncionarioAsync(int funcionarioId)
+    {
+        if(!await _funcionarioRepository.ExisteAsync(funcionarioId)) return null;
+        return await _repository.ListarPorFuncionarioAsync(funcionarioId);
+    }
+
+    public Task<Endereco?> ObterPorIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Endereco?> CriarAsync(int funcionarioId, Endereco endereco)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Endereco?> AtualizarAsync(int id, Endereco endereco)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoverAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
--- API-31-08/Services/SetorService.cs
+++ API-31-08/Services/SetorService.cs
@@ -1,5 +1,6 @@
 ﻿using API_31_08.Infra;
 using API_31_08.Models;
+using API_31_08.Repositories;
 
 namespace API_31_08.Services;
 
@@ -9,43 +10,47 @@ public interface ISetorService
     Task<Setor?> ObterPorIdAsync(int id);
     Task<Setor> CriarAsync(Setor setor);
     Task<bool> AtualizarAsync(int id, Setor setor);
-    Task<(bool Removido, string? Erro)> ExcluirAsync(int id);
+    Task<(bool Removido, string? Erro)> RemoverAsync(int id);
 }
 
 public class SetorService : ISetorService
 {
-    private readonly AppDbContext _repository;
-    public SetorService(AppDbContext repository)
-    {
-        _repository = repository;
-    }
-    public Task<List<Setor>> ListarAsync()
-    {
-        throw new NotImplementedException();
-    }
+    private readonly ISetorRepository _repository;
 
-    public Task<Setor?> ObterPorIdAsync(int id)
+    public SetorService(ISetorRepository setorRepository)
     {
-        
+        _repository = setorRepository;
     }
+    
+    public Task<List<Setor>> ListarAsync() => _repository.ListarAsync();
+
+    public Task<Setor?> ObterPorIdAsync(int id) => _repository.ObterPorIdAsync(id);
 
     public async Task<Setor> CriarAsync(Setor setor)
     {
-        await _repository.AddAsync(setor);
+        await _repository.AdicionarAsync(setor);
+        return setor;
     }
 
     public async Task<bool> AtualizarAsync(int id, Setor setor)
     {
         var existente = await _repository.ObterPorIdAsync(setor.Id);
         if (existente == null) return false;
+        
+        existente.Nome = setor.Nome;
+        await _repository.AtualizarAsync(existente);
+        return true;
     }
 
-    public async Task<(bool Removido, string? Erro)> ExcluirAsync(int id)
+    public async Task<(bool Removido, string? Erro)> RemoverAsync(int id)
     {
         var setor = await _repository.ObterPorIdAsync(id);
         if (setor is null) return (false, null);
 
-        if (setor.Funcionario.Any())
-            return (false, "Não é possivel remover um setor que possui funcionario.");
+        if (setor.Funcionarios.Any())
+            return (false, "Nao é possivel remover um setor que possui funcionarios vinculados");
+
+        await _repository.RemoverAsync(setor);
+        return (true, null);
     }
 }
