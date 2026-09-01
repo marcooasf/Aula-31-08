11226ccd86882b339b80b757be88343d82ac1b13
Cursos Oficiais Microsoft <moc@proway.treina>
2026-08-31 21:43:24 -0300

    novo

--- API-31-08/Controllers/EnderecoController.cs
+++ API-31-08/Controllers/EnderecoController.cs
@@ -7,15 +7,28 @@ using Microsoft.JSInterop;
 namespace API_31_08.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
-public class EnderecoController
+[Route("api/funcionario/{FuncionarioId:int}/[controller]")]
+public class EnderecoController :  ControllerBase
 {
-   private readonly IEnderecoService _enderecoService;
+   private readonly IEnderecoService _service;
    
-   public EnderecoController(IEnderecoService enderecoService) => _enderecoService = enderecoService;
+   public EnderecoController(IEnderecoService enderecoService) => _service = enderecoService;
 
-   /*[HttpGet]
-   public async Task<ActionResult<List<Endereco>?>> ListarPorFuncionario(int funcionarioId) =>
-   Ok(await _enderecoService.ListarPorFuncionarioAsync(funcionarioId));*/
+   [HttpGet]
+   public async Task<ActionResult<List<Endereco>?>> ListarPorFuncionario(int funcionarioId)
+   {
+      var enderecos = await _service.ListarPorFuncionarioAsync(funcionarioId);
+      return enderecos is null ? NotFound() : Ok(enderecos);
+   }
+
+   [HttpGet("{id}", Name = "ObterEndereco")]
+   public async Task<ActionResult<Endereco>> Obter(int funcionarioId, int id)
+   {
+      var endereco = await _service.ObterAsync(id);
+      
+      if(endereco is null || endereco.FuncionarioId != funcionarioId)
+         return NotFound();
+      return Ok(endereco);
+   }
    
 }
